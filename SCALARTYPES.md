8f6d501a7f5269bae71e76b09f888a57aaaa93c9
Francisco Macedo <macedolb53@gmail.com>
2024-04-05 10:43:59 +0200

    Scalares
    Definição de tipos escalares no Graphql

--- SCALARTYPES.md
+++ SCALARTYPES.md
@@ -7,5 +7,17 @@
     * Boolean
     * ID
 
+***
+
+# const typeDefs = gql``
+
+    * Aqui, estamos definindo nossos tipos GraphQL usando a função gql que importamos anteriormente. No entanto, neste exemplo, não estamos definindo nenhum tipo. Essa definição de tipo será preenchida posteriormente com os tipos GraphQL reais do nosso esquema.
+
+# const resolvers = {}
+
+    * Estamos definindo um objeto vazio chamado resolvers. Os resolvers são responsáveis por fornecer a funcionalidade para cada campo nos tipos definidos em typeDefs. Neste exemplo, não estamos definindo nenhum resolver ainda.
+
+***
+
 ### gql: É uma função que nos permite escrever nossos esquemas GraphQL usando uma sintaxe especial, que é transformada em um objeto JavaScript compreensível pelo Apollo Server.
 ### ApolloServer: É a classe principal do Apollo Server, que usaremos para configurar e iniciar o servidor GraphQL.
--- aulas/a02.js
+++ aulas/a02.js
@@ -1,10 +1,41 @@
 //Nesta aula vamos ver os tipos scalares do graphql
 const {gql, ApolloServer} = require("apollo-server");
 
-const typeDefs = gql``
 
-//O codigo abaixa representa uma class em JavaScript
-const resolvers = {}
+//Definicao dos atributos
+const typeDefs = gql`
+    type Query{
+        idade: Int
+        salario: Float
+        nome: String
+        ativo: Boolean
+        id: ID
+    }
+
+`;
+
+//O codigo abaixa representa uma class em JavaScript, é aqui onde atribuinos valores aos tipos definidos no typeDefs, em outras palavras aos nosso metodos
+const resolvers = {
+    Query:{
+        idade(){
+            return 18;
+        },
+        salario(){
+            return 10000.000; 
+        },
+        nome(){
+            return 'Miguel';
+        },
+        ativo(){
+            return true;
+        },
+        id(){
+            return 12123234;
+        }
+
+    }
+
+};
 
 
 const server = new ApolloServer({//instancia
